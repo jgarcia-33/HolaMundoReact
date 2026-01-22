31148b4da316705403b7bfd413dfdbaf15881a9f
jgarcia-33 <jgarciatorices@gmail.com>
2026-01-22 12:03:31 +0100

    initial commit

---
+++ holamundo/src/components/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Holaaa</h1>
+    </>
+  )
+}
+
+export default App
---
+++ holamundo/src/components/List.tsx
@@ -0,0 +1,13 @@
+function List() {
+  let items = ["Leche", "Huevos", "Carne", "Pan", "Mantequilla", "Queso"];
+  return (
+    <div>
+      <ul class="list-group">
+        {items.map((item) => (<}
+
+      </ul>
+    </div>
+  );
+}
+
+export default List;
