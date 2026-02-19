8d5d4b32bbd250195ffcb1e6efa0b3a4c8a02d47
jgarcia-33 <jgarciatorices@gmail.com>
2026-02-19 12:45:43 +0100

    add ods filter

--- holamundo/src/main.tsx
+++ holamundo/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import List from "./components/List.tsx";
 import HolaReact from "./components/HolaReact.tsx";
+import Ods from "./components/Ods.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HolaReact />
-    <List />
+    {/* <HolaReact /> */}
+    <Ods />
+    {/* <List /> */}
   </StrictMode>,
 );
