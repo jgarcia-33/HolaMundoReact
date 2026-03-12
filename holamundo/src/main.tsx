83b28fa35acf0223d578b9b8785fafe184dcd7b5
jgarcia-33 <jgarciatorices@gmail.com>
2026-03-12 12:36:56 +0100

    Estructura final del proyecto

--- holamundo/src/main.tsx
+++ holamundo/src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import List from "./components/List.tsx";
-import HolaReact from "./components/HolaReact.tsx";
-import Ods from "./components/Ods.tsx";
+import App from "./components/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <HolaReact /> */}
-    <Ods />
-    {/* <List /> */}
+    <App />
   </StrictMode>,
 );
